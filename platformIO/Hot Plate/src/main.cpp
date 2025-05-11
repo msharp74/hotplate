891a222f991757254e1afe659d80ec26c0e2bcdb
Matt Sharp <matt.sharp2@gmail.com>
2025-05-11 16:57:02 -0400

    SPI functionality

--- platformIO/Hot Plate/src/main.cpp
+++ platformIO/Hot Plate/src/main.cpp
@@ -1,18 +1,43 @@
 #include <Arduino.h>
+#include <SPI.h>
 
-// put function declarations here:
-int myFunction(int, int);
+uint8_t returnVal {0};
+uint8_t data[3] {0};
+uint8_t csPin {5};
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600);
+  SPI.begin();
+  data[0] = 0b00000110;
+  data[1] = 0;
+  data[2] = 0;
+  pinMode(csPin, OUTPUT);
+  digitalWrite(csPin, HIGH);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-}
+  SPI.beginTransaction(SPISettings(14000000, MSBFIRST, SPI_MODE0));
+  digitalWrite(csPin, LOW);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  data[0] = 0b00000110;
+  data[1] = 0;
+  data[2] = 0;
+
+  SPI.transfer(data, 3);
+  Serial.print("Data First Index: \t");
+  Serial.println(data[0], BIN);
+  Serial.print("Data Second Index: \t");
+  Serial.println(data[1], BIN);
+  Serial.print("Data Third Index: \t");
+  Serial.println(data[2], BIN);
+
+  unsigned int voltageBits {0};
+  
+
+  digitalWrite(csPin, HIGH);
+  SPI.endTransaction();
+
+  sleep(2);
 }
