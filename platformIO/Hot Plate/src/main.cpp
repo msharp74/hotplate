c451be97f53624cd219c68a7386c89029cec3cde
Matt Sharp <matt.sharp2@gmail.com>
2025-06-05 00:07:52 -0400

    Creating reflow class

--- platformIO/Hot Plate/src/main.cpp
+++ platformIO/Hot Plate/src/main.cpp
@@ -10,22 +10,48 @@ uint8_t data[3] {0};
 MCP3204 adc {csPin};
 PID pid {0.1, 0.1, 0};
 
-struct ReflowSubsectionDetails
-{
-  int temp;        //in degrees celsius
-  int duration;    //in seconds
-};
 
-enum ReflowSubsection
+class ReflowProfile
 {
-  preheat,
-  soak,
-  reflow,
-  numSubsections
+  private:
+    struct ReflowSection
+    {
+      int temp;
+      int timePeriod;
+    };
+    ReflowSection m_tempProfileArray[3] {};
+    int timeArray[100];
+    double tempArray[100];
+
+  public:
+    ReflowProfile(ReflowSection tempProfileArray[3])
+    {
+      int arrayLength {0};
+      for(int i {0}; i < 3; i ++)
+      {
+        m_tempProfileArray[i] = tempProfileArray[i];
+        arrayLength += tempProfileArray[i].timePeriod;
+      }
+      int *timeArray = new int [arrayLength];
+      double *tempArray = new double [arrayLength];
+
+      for (int profileIndex {0}; profileIndex > 3; profileIndex++)
+      {
+        for (int index {(profileIndex == 0 ? 0 : m_tempProfileArray[profileIndex - 1].timePeriod)}; index < m_tempProfileArray[profileIndex].timePeriod; index++)
+        {
+          timeArray[index];
+        }
+      }
+    }
+
+    ReflowSection getRelowProfilePeriod (int index)
+    {
+      return m_tempProfileArray[index];
+    }
 };
 
 //define all reflow profiles for any solder paste that is to be used
-ReflowSubsectionDetails SN42BI58[ReflowSubsection::numSubsections] {(80, 120), (110, 120), (155, 35)};
+//ReflowSubsectionDetails SN42BI58[ReflowSubsection::numSubsections] {(80, 120), (110, 120), (155, 35)};
 
 
 void setup()
